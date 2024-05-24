6e9cd33d861f87c5425bef0ce09a1f2005dcc33d
Madhur1707 <madhurpathak000@gmail.com>
2024-05-24 11:56:50 +0530

    prePoulated User Data in user profile page

--- api/src/routes/MyUserRoute.ts
+++ api/src/routes/MyUserRoute.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 
 // /api/my/user
 
+router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
 router.put("/", jwtCheck, jwtParse, MyUserController.updateCurrentUser);
 
