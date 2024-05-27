e8823b13613f2e936c53e9309a332507359d3b8e
Madhur1707 <madhurpathak000@gmail.com>
2024-05-27 11:30:36 +0530

    Restaurant Controller

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,17 +3,29 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import MyUserRoute from "./routes/MyUserRoute";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import { v2 as cloudinary } from "cloudinary";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to database!"));
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/my/user", MyUserRoute);
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health ok!" });
+});
 
+app.use("/api/my/user", MyUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 app.listen(7000, () => {
   console.log("Server Started on localhost: 7000");
 });
