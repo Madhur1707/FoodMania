4969efbcb3f910cd9404a6899b5b32cd42cb0e4e
Madhur1707 <madhurpathak000@gmail.com>
2024-05-31 13:00:55 +0530

    Restaurant Page

---
+++ client/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -0,0 +1,67 @@
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import DetailsSection from "./DetailsSection";
+
+const formSchema = z.object({
+  restaurantName: z.string({
+    required_error: "Restaurant Name is required",
+  }),
+  city: z.string({
+    required_error: "City is required",
+  }),
+  country: z.string({
+    required_error: "Country is required",
+  }),
+  deliveryPrice: z.coerce.number({
+    required_error: "Delivery Price is required",
+    invalid_type_error: "must be a number",
+  }),
+  estimatedDeliveryTime: z.coerce.number({
+    required_error: "Estimated Delivery Time is required",
+    invalid_type_error: "must be a number",
+  }),
+  cuisines: z.array(z.string()).nonempty({
+    message: "please select atleast one item",
+  }),
+  menuItems: z.array(
+    z.object({
+      name: z.string().min(1, "name is required"),
+      price: z.coerce.number().min(1, "price is required"),
+    })
+  ),
+  imageFile: z.instanceof(File, { message: "Image is required" }),
+});
+
+type restaurantFormData = z.infer<typeof formSchema>;
+
+type Props = {
+  onsave: (restaurantFormData: FormData) => void;
+  isLoading: boolean;
+};
+
+const ManageRestaurantForm = ({ onsave, isLoading }: Props) => {
+  const form = useForm<restaurantFormData>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      cuisines: [],
+      menuItems: [{ name: "", price: 0 }],
+    },
+  });
+  const onSubmit = (formDataJson: restaurantFormData) => {
+    // TODO: convert formDataJson to FormData Object
+  };
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8 bg-gray-50 p-10 rounded-lg"
+      >
+        <DetailsSection />
+      </form>
+    </Form>
+  );
+};
+
+export default ManageRestaurantForm;
