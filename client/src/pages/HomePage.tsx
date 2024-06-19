7d7bd8655ac0cf60631f29d5f5558013de68926d
Madhur1707 <madhurpathak000@gmail.com>
2024-06-19 16:12:35 +0530

    Searchapi for  UI and getting data on ui

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,31 +1,45 @@
+import SearchBar, { SearchForm } from "@/components/SearchBar";
 import landingpage from "../assets/landingpage.png";
 import playStore from "../assets/playStore.png";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-    return (
-        <div className="flex flex-col gap-12">
-            <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
-                <h1 className="text-5xl font-bold tracking-tight text-orange-600">
-                    {" "}
-                    Tuck into a takeway today..
-                </h1>
-                <span className="text-xl">Food is just a click away!</span>
-            </div>
-            <div className="grid md:grid-cols-2 gap-5">
-                <img src={landingpage} />
-                <div className="flex flex-col items-center justify-center gap-4 text-center">
-                    <span className="font-bold text-3xl tracking-tighter">
-                        Order Takeaway even faster!
-                    </span>
-                    <span>
-                        Download the FoodMania app for faster ordering and personalized
-                        recommendations..
-                    </span>
-                    <img src={playStore} />
-                </div>
-            </div>
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValue: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValue.searchQuery}`,
+    });
+  };
+
+  return (
+    <div className="flex flex-col gap-12">
+      <div className=" md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+        <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+          {" "}
+          Tuck into a takeway today..
+        </h1>
+        <span className="text-xl">Food is just a click away!</span>
+        <SearchBar
+          placeHolder="Search by city or town"
+          onSubmit={handleSearchSubmit}
+        />
+      </div>
+      <div className="grid md:grid-cols-2 gap-5">
+        <img src={landingpage} />
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tighter">
+            Order Takeaway even faster!
+          </span>
+          <span>
+            Download the FoodMania app for faster ordering and personalized
+            recommendations..
+          </span>
+          <img src={playStore} />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ client/src/pages/SearchPage.tsx
@@ -0,0 +1,21 @@
+import { useSearchRestaurants } from "@/api/MyRestaurantApi";
+import { useParams } from "react-router-dom";
+
+const SearchPage = () => {
+  const { city } = useParams();
+  const { results } = useSearchRestaurants(city);
+  return (
+    <span>
+      user searched for {city}{" "}
+      <span>
+        {results?.data.map((restaurant) => (
+          <span>
+            found - {restaurant.restaurantName}, {restaurant.city}
+          </span>
+        ))}
+      </span>{" "}
+    </span>
+  );
+};
+
+export default SearchPage;
